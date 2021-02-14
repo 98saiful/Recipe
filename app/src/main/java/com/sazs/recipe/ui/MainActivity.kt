127869a493f76e2feaf802d818077c765d561c2f
Saiful Amir <saifulamir98@gmail.com>
2021-02-14 20:04:23 +0800

    New splash screen and icon

--- app/src/main/java/com/sazs/recipe/ui/MainActivity.kt
+++ app/src/main/java/com/sazs/recipe/ui/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_Recipe)
         setContentView(binding.root)
 
         navController = findNavController(R.id.navHostFragment)
